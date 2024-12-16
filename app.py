78305373d3405f319e03bd21e744dabeef50c9d9
Sandeepkumar1921 <deepjagrot@gmail.com>
2024-12-16 20:15:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from flask import Flask, request, jsonify, render_template
 import yt_dlp
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
-CORS(app)  # CORS ko enable kiya gaya hai
+CORS(app)  # Enable CORS for cross-origin requests
+
+# Ensure 'downloads' folder exists
+if not os.path.exists('downloads'):
+    os.makedirs('downloads')
 
 @app.route('/')
 def home():
@@ -14,7 +19,7 @@ def download_video():
     video_url = request.form.get('video_url')
     
     if not video_url:
-        return jsonify({'error': 'No URL provided'}), 400  # Bad Request agar URL nahi diya hai
+        return jsonify({'error': 'No URL provided'}), 400  # Bad Request if URL not provided
 
     ydl_opts = {
         'format': 'best',
@@ -29,6 +34,3 @@ def download_video():
             return jsonify({'message': f'Video "{video_title}" downloaded successfully!'})
     except Exception as e:
         return jsonify({'error': str(e)}), 500  # Internal Server Error
-
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)  # Flask ko public IP pe run kareng
